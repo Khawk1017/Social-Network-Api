7d86468b90c3049e1751a34437b2e28f1c07b3d4
Kameron Hawk <yungkam@Kamerons-MacBook-Pro.local>
2023-05-14 17:21:29 -0700

    exported the user and thought models

--- models/index.js
+++ models/index.js
@@ -1,4 +1,5 @@
 const User = require('./User');
 const Thought = require('./Thought');
-
+// exporting the 'User' and 'Thought' model from correspoing files to be reusable throughout the applocation 
 module.exports = { User, Thought}
+
