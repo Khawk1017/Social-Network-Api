cfd54625af91624d13fd0178054811389fb59cb2
Kameron Hawk <yungkam@Kamerons-MacBook-Pro.local>
2023-05-14 17:19:43 -0700

    user contoller file

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,127 @@
 const { ObjectId } = require('mongoose').Types;
-const { User } = require('../models')
+const { User, Thought } = require('../models');
 
+
+//  Aggregate function to get the number of Users 
+const headCount = async () => {
+    const numberOfUsers = await User.aggregate()
+        .count('userCount');
+    return numberOfUsers;
+}
+
+module.exports = {
+    // Get all users 
+    async getUsers(req, res) {
+        try {
+            const users = await User.find();
+
+            const userObj = {
+                users,
+                headCount: await headCount(),
+            };
+
+            res.json(userObj);
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json(err);
+        }
+    },
+    // Get a single user 
+    async getSingleUser(req, res) {
+        try {
+            const user = await User.findOne({ _id: req.params.userId })
+                .select('__v');
+
+            if (!user) {
+                return res.status(404).json({ message: 'No user witht hat ID' })
+            }
+
+            res.json({
+                user
+            });
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json(err);
+        }
+    },
+    // Create a new user
+    async createUser(req, res) {
+        try {
+            const user = await User.create(req.body);
+            res.json(user);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    // Delete a user and remove them from their email
+    async deleteUser(req, res) {
+        try {
+            const user = await User.findOneAndRemove({ _id: req.params.userId });
+
+            if (!user) {
+                return res.status(404).json({ message: 'No User exists' });
+            }
+
+            const thought = await Thought.findOneAndUpdate(
+                { users: req.params.userId },
+                { $pull: { users: req.params.userId } },
+                { new: true }
+            );
+
+            if (!thought) {
+                return res.status(404).json({
+                    message: 'User deleted, but no thought found',
+                });
+            }
+
+            res.json({ message: 'User successfully deleted' });
+        } catch (err) {
+            console.log(err);
+            res.status(500).json(err);
+        }
+    },
+
+    // Add reaction to the user 
+    async addReaction(req, res) {
+        console.log('You are adding an thought');
+        console.log(req.body);
+
+        try {
+            const user = await User.findOneAndUpdate(
+                { _id: req.params.userId },
+                { $addToSet: { reactions: req.body } },
+                { runValidators: true, new: true }
+            );
+
+            if (!user) {
+                return res
+                    .status(404)
+                    .json({ message: 'No user found with that ID :(' });
+            }
+
+            res.json(user);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    // Removes reaction from a user 
+    async removeReaction(req, res) {
+        try {
+            const user = await User.findOneAndUpdate(
+                { _id: req.params.userId},
+                { $pull: { reactions: {reactionId: req.params.reactionId} } },
+                { runValidators: true, new: true}
+            );
+
+            if (!user) {
+                return res
+                .status(404)
+                .json({ message: 'No User with that ID ;('});
+            }
+
+            res.json(user);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+};
