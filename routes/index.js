8478d50e8ef9395382e2011cbd44e2f782b64a2f
Kameron Hawk <yungkam@Kamerons-MacBook-Pro.local>
2023-05-14 17:26:52 -0700

    set up the router using express

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
+// setting up the router using the express framework
 const router = require('express').Router();
 const apiRoutes = require('./api');
-
+// using the  apiRoutes module for routes starting with '/api'
 router.use('/api', apiRoutes);
 
+// if the request does not match any defined routes will send this response message
 router.use((req, res) => res.send('Wrong route!'));
 
+// exporing the router module to be reusable in application
 module.exports = router;
