109d3d3283d4829cb814eea996b3ec775bb10cf5
Kameron Hawk <yungkam@Kamerons-MacBook-Pro.local>
2023-05-14 18:40:53 -0700

    routes created for the thought model

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+const {
+    getThought,
+    getSingleThought,
+    createThought,
+    deleteThought,
+    updateThought,
+    addReaction,
+    removeReaction,
+} = require('../../controllers/thoughtsController')
+
+router.route('/').get(getThought).post(createThought);
+
+router
+    .route("/:id")
+    .get(getSingleThought)
+    .put(updateThought)
+    .delete(deleteThought);
+
+    router.route("/thoughtId/reactions").post(addReaction);
+
+    router.route("/:thoughtId/reactions/reactionId").delete(removeReaction);
+
+    module.exports = router;
