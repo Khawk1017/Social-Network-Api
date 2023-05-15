44467c905dadf5b493a7d215ec69d48b3d7de574
Kameron Hawk <yungkam@Kamerons-MacBook-Pro.local>
2023-05-14 19:38:13 -0700

    resolved my schema config

--- models/Reaction.js
+++ models/Reaction.js
@@ -1,40 +1,37 @@
-const { Schema, Types, model} = require('mongoose');
-
+const { Schema, Types, model } = require("mongoose");
 const reactionSchema = new Schema(
-    {
-        reactionId: {
-            type: Schema.Types.ObjectId,
-            default: () => new Types.ObjectId(),
-        },
-        reactionBody: {
-            type: String,
-            required: true,
-            maxlength: 280,
-        },
-        username: {
-            type: String,
-            required: true,
-            unique: true
-        },
-        createdAt: {
-            type: Date,
-            required: true,
-            unique: true,
-            match: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-            default: Date.now,
-        },
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
     },
-    {
-        toJSON: {
-            getters: true,
-        },
-        id: false,
-    }
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    createdAt: {
+      type: Date,
+      required: true,
+      unique: true,
+      match: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      default: Date.now,
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
 );
-reactionSchema.virtual('formattedCreatedAt').get(function () {
-    return this.createdAt.toLocaleDateString()
+reactionSchema.virtual("formattedCreatedAt").get(function () {
+  return this.createdAt.toLocaleDateString();
 });
-
-const Reaction = model('Reaction', reactionSchema)
-
-module.exports = Reaction;
+// const Reaction = model('Reaction', reactionSchema)
+module.exports = reactionSchema;
